80eed94c41deea32c756ed495b3bd9f83f083672
suzuken0424 <s.kenta0424@gmail.com>
2024-06-14 07:26:46 +0900

    b09完了

--- tessoku-book/a09/main.cpp
+++ tessoku-book/a09/main.cpp
@@ -1,5 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int H, W, N;
+int A[100009], B[100009], C[100009], D[100009];
+int X[1509][1509], T[1509][1509]; 
 int main()
 {
-    // code
+    cin >> H >> W >> N;
+    for(int i = 1; i <= N; i++) cin >> A[i] >> B[i] >> C[i] >> D[i];
+
+    // 四隅に+1,-1を付与
+    for(int i = 1; i <= N; i++) {
+        X[A[i]][B[i]]++;
+        X[C[i] + 1][D[i] + 1]++;
+        X[A[i]][D[i] + 1]--;
+        X[C[i] + 1][B[i]]--;
+    }
+
+    // 累積和の計算
+    // 横方向の計算
+    for(int i = 1; i <= H; i++) {
+        for(int j = 1; j <= W; j++) {
+            T[i][j] = T[i][j - 1] + X[i][j];
+        }
+    }
+    // 横方向の計算
+    for(int j = 1; j <= W; j++) {
+        for(int i = 1; i <= H; i++) {
+            T[i][j] = T[i - 1][j] + T[i][j];
+        }
+    }
+
+    // 回答
+    for(int i = 1; i <= H; i++) {
+        for(int j = 1; j <= W; j++) {
+            if(j >= 2) cout << " ";
+            cout << T[i][j];
+        }
+        cout << endl;
+    }
+
+
     return 0;
 }
--- tessoku-book/b09/main.cpp
+++ tessoku-book/b09/main.cpp
@@ -1,5 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int N;
+int A[100009], B[100009], C[100009], D[100009];
+int X[1509][1509], Z[1509][1509]; 
 int main()
 {
-    // code
+    cin >> N;
+    for(int i = 1; i <= N; i++) cin >> A[i] >> B[i] >> C[i] >> D[i];
+
+    // 四隅に+1,-1を付与
+    // https://atcoder.jp/contests/tessoku-book/submissions/53668223を参考にした
+    // 0の部分の計算がしにくいので、x,y両方向に１ずつ移動させる
+    for(int i = 1; i <= N; i++) {
+        X[A[i]+1][B[i]+1]++;
+        X[A[i]+1][D[i]+1]--;
+        X[C[i]+1][B[i]+1]--;
+        X[C[i]+1][D[i]+1]++;
+    }
+
+    // 累積和の計算
+    // 横方向の計算
+    for(int i = 1; i <= 1500; i++) {
+        for(int j = 1; j <= 1500; j++) {
+            Z[i][j] = Z[i][j - 1] + X[i][j];
+        }
+    }
+    // 横方向の計算
+    for(int i = 1; i <= 1500; i++) {
+        for(int j = 1; j <= 1500; j++) {
+            Z[i][j] = Z[i - 1][j] + Z[i][j];
+        }
+    }
+
+    // 回答
+    int Answer = 0;
+    for(int i = 0; i <= 1500; i++) {
+        for(int j = 0; j <= 1500; j++) {
+            if(Z[i][j] >= 1) Answer++;
+        }
+    }
+    cout << Answer << endl;
+
     return 0;
 }
