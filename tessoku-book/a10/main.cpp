5b8516320d14c4a4002f439373e04cbce030bf07
suzuken0424 <s.kenta0424@gmail.com>
2024-06-15 07:34:49 +0900

    a10完了

--- tessoku-book/a10/main.cpp
+++ tessoku-book/a10/main.cpp
@@ -1,5 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int N, D;
+int A[100009], L[100009], R[100009], LT[100009], RT[100009];
 int main()
 {
-    // code
+    cin >> N;
+    for(int i = 1; i <= N; i++) cin >> A[i];
+    cin >> D;
+    for(int i = 1; i <= D; i++) cin >> L[i] >> R[i];
+
+    LT[1] = A[1];
+    for(int i = 2; i <= N; i++) {
+        // LT[i] = max(A[i], LT[i - 1]);
+        if(LT[i - 1] < A[i]) LT[i] = A[i];
+        else LT[i] = LT[i - 1];
+    }
+
+    RT[N] = A[N];
+    for(int i = N-1; i >= 1; i--) {
+        // RT[i] = max(A[i], RT[i + 1]);
+        if(RT[i + 1] < A[i]) RT[i] = A[i];
+        else RT[i] = RT[i + 1];
+    }
+
+    for(int i = 1; i <= D; i++) {
+        cout << max(LT[L[i] - 1], RT[R[i] + 1]) << endl;
+    }
+    
     return 0;
 }
