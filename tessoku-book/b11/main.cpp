0a888bafa8e9604ecd66607e54fee3221841c013
suzuken0424 <s.kenta0424@gmail.com>
2024-06-15 21:36:21 +0900

    b11完了

--- tessoku-book/b11/main.cpp
+++ tessoku-book/b11/main.cpp
@@ -1,5 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, Q, A[100009], X[100009];
 int main()
 {
-    // code
+    cin >> N;
+    for(int i = 1; i <= N; i++) cin >> A[i];
+    cin >> Q;
+    for(int i = 1; i <= Q; i++) cin >> X[i];
+
+    sort(A + 1, A + N + 1);
+
+    for(int i = 1; i <= Q; i++) {
+        int pos = lower_bound(A + 1, A + N + 1, X[i]) - A;
+        cout << pos - 1 << endl;
+    }
+    
     return 0;
 }
